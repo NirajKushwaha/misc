fa67c65c6f470f84db1e11b7ac0a704d2ef9211b
niraj <nirajkkushwaha1@gmail.com>
2024-09-06 14:54:30 +0200

    added more functions in info_theory

--- info_theory.py
+++ info_theory.py
@@ -23,3 +23,177 @@ def data_entropy(x, normalized=False):
         return -entropy/np.log2(len(counts))
     else:
         return -entropy
+    
+def mutual_information(time_series_1, time_series_2):
+    """
+    Calculates mutual information between two discrete time series.
+    
+    Parameter
+    ---------
+    time_series_1 : list like
+    time_series_2 : list like
+    
+    Returns
+    -------
+    float
+    """
+    
+    assert len(time_series_1) == len(time_series_2)
+    
+    joint_events = list(zip(time_series_1, time_series_2))
+
+    joint_prob = Counter(joint_events)
+    total_events = len(joint_events)
+
+    prob_time_series_1 = Counter(time_series_1)
+    prob_time_series_2 = Counter(time_series_2)
+
+    mutual_info = 0.0
+
+    for (i, j), count in joint_prob.items():
+        prob_joint = count / total_events
+
+        prob_x = prob_time_series_1[i] / total_events
+        prob_y = prob_time_series_2[j] / total_events
+        mutual_info += prob_joint * np.log2(prob_joint / (prob_x*prob_y))
+
+    return mutual_info
+
+def normalized_mutual_information(time_series_1, time_series_2):
+    """
+    Calculates normalized mutual information (geometric) between two discrete time series.
+
+    Parameter
+    ---------
+    time_series_1 : list like
+    time_series_2 : list like
+    
+    Returns
+    -------
+    float
+    """
+    
+    assert len(time_series_1) == len(time_series_2)
+    
+    joint_events = list(zip(time_series_1, time_series_2))
+
+    joint_prob = Counter(joint_events)
+    total_events = len(joint_events)
+
+    prob_time_series_1 = Counter(time_series_1)
+    prob_time_series_2 = Counter(time_series_2)
+
+    mutual_info = 0.0
+
+    for (i, j), count in joint_prob.items():
+        prob_joint = count / total_events
+
+        prob_x = prob_time_series_1[i] / total_events
+        prob_y = prob_time_series_2[j] / total_events
+        mutual_info += prob_joint * np.log2(prob_joint / (prob_x*prob_y))
+
+    entropy_1 = data_entropy(time_series_1)
+    entropy_2 = data_entropy(time_series_2)
+
+    normalized_mutual_info = mutual_info / np.sqrt(entropy_1*entropy_2)
+
+    return normalized_mutual_info
+
+def joint_entropy(time_series_1, time_series_2):
+    """
+    Calculate joint shannon entropy of series data.
+    
+    Parameters
+    ----------
+    time_series_1 : list like
+    time_series_2 : list like
+    
+    Return
+    ------
+    float
+    """
+    
+    joint_events = list(zip(time_series_1, time_series_2))
+
+    joint_prob = Counter(joint_events)
+    total_events = len(joint_events)
+
+    joint_entropy = 0.0
+
+    for (i, j), count in joint_prob.items():
+        prob_joint = count / total_events
+
+        joint_entropy += prob_joint * np.log2(prob_joint)
+
+    return -joint_entropy
+
+def conditional_entropy(time_series_1, time_series_2, normalized=False):
+    """
+    Calculates conditional entropy for time_series_2 given time_series_1. 
+    
+    Parameter
+    ---------
+    time_series_1 : list like
+    time_series_2 : list like
+    normalized : bool, False
+    
+    Returns
+    -------
+    float
+    """
+    
+    assert len(time_series_1) == len(time_series_2)
+    
+    joint_events = list(zip(time_series_1, time_series_2))
+
+    joint_prob = Counter(joint_events)
+    total_events = len(joint_events)
+
+    prob_time_series_1 = Counter(time_series_1)
+    prob_time_series_2 = Counter(time_series_2)
+
+    cond_entro = 0.0
+
+    for (i, j), count in joint_prob.items():
+        prob_joint = count / total_events
+
+        prob_x = prob_time_series_1[i] / total_events
+        cond_entro += prob_joint * np.log2(prob_joint / prob_x)
+
+    if(normalized):
+        return - cond_entro / data_entropy(time_series_2)
+    else:
+        return - cond_entro
+
+def KL_divergence_prob(P, Q):
+    """
+    Calculates KL divergence between discrete probability distributions P and Q (P||Q).
+
+    Parameters
+    ----------
+    P : ndarray
+    Q : ndarray
+
+    Returns
+    -------
+    float
+    """
+
+    return np.sum(P * np.log(P / Q))
+
+def JS_divergence_prob(P, Q):
+    """
+    Calculate Jensen-Shannon entropy between discrete probability distributions P and Q.
+
+    Parameters
+    ----------
+    P : ndarray
+    Q : ndarray
+
+    Returns
+    -------
+    float
+    """
+
+    mixture = 0.5 * (P + Q)
+    return 0.5 * (KL_divergence_prob(P, mixture) + KL_divergence_prob(Q, mixture))
