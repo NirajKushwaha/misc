bca95dbd5662ba210b193e2e9a2e8f35db7f4490
niraj <nirajkkushwaha1@gmail.com>
2024-09-09 12:08:24 +0200

    made normalization function comapatible with np.nan values

--- list_operations.py
+++ list_operations.py
@@ -2,24 +2,29 @@ from utils import *
 
 def normalize_to_range(lst, x, y):
     """
-    Normalize a list of values to a given range.
+    Normalize a list of values to a given range, ignoring np.nan values.
 
     Parameters
     ----------
-    lst : list like
+    lst : list-like
     x : float
     y : float
 
-    Return
-    ------
-    list
+    Returns
+    -------
+    ndarray
+        A ndarray of normalized values within the range [x, y], with np.nan values unchanged.
     """
 
-    min_val = min(lst)
-    max_val = max(lst)
-    
-    # Edge case: if all values are the same, return a list of `x`
-    if min_val == max_val:
-        return [x] * len(lst)
+    arr = np.array(lst, dtype=np.float64)
 
-    return [(y - x) * (val - min_val) / (max_val - min_val) + x for val in lst]
+    min_val = np.nanmin(arr)
+    max_val = np.nanmax(arr)
+
+    # Edge case: if all non-nan values are the same, return a list of `x`, preserving np.nan values
+    if min_val == max_val:
+        return [x if not np.isnan(val) else np.nan for val in arr]
+
+    normalized = [(y - x) * (val - min_val) / (max_val - min_val) + x if not np.isnan(val) else np.nan for val in arr]
+
+    return np.array(normalized)
