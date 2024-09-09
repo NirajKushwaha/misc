b2f2dce451474ad38922f4b1b9dde6aec4af7d5e
niraj <nirajkkushwaha1@gmail.com>
2024-09-09 17:53:34 +0200

    turned mutual information into a  Class

--- info_theory.py
+++ info_theory.py
@@ -26,53 +26,6 @@ def data_entropy(x, normalized=False):
             return -entropy/np.log2(len(counts))
         else:
             return -entropy
-    
-def mutual_information(time_series_1, time_series_2, bootstrap=False, num_of_trials=1000):
-    """
-    Calculates mutual information between two discrete time series.
-    
-    Parameter
-    ---------
-    time_series_1 : list like
-    time_series_2 : list like
-    bootstrap : bool, False
-    num_of_trials : int, 1000
-    
-    Returns
-    -------
-    float
-    """
-    
-    assert len(time_series_1) == len(time_series_2)
-    
-    joint_events = list(zip(time_series_1, time_series_2))
-
-    joint_prob = Counter(joint_events)
-    total_events = len(joint_events)
-
-    prob_time_series_1 = Counter(time_series_1)
-    prob_time_series_2 = Counter(time_series_2)
-
-    mutual_info = 0.0
-
-    for (i, j), count in joint_prob.items():
-        prob_joint = count / total_events
-
-        prob_x = prob_time_series_1[i] / total_events
-        prob_y = prob_time_series_2[j] / total_events
-        mutual_info += prob_joint * np.log2(prob_joint / (prob_x*prob_y))
-
-    if(bootstrap):
-        mutual_info_trials = []
-        for _ in range(num_of_trials):
-            time_series_1_boot = np.random.choice(time_series_1, len(time_series_1), replace=True)
-            time_series_2_boot = np.random.choice(time_series_2, len(time_series_2), replace=True)
-            mutual_info_boot = mutual_information(time_series_1_boot, time_series_2_boot)
-            mutual_info_trials.append(mutual_info_boot)
-
-        return mutual_info, mutual_info_trials
-    else:
-        return mutual_info
 
 def normalized_mutual_information(time_series_1, time_series_2):
     """
@@ -212,3 +165,90 @@ def JS_divergence_prob(P, Q):
 
     mixture = 0.5 * (P + Q)
     return 0.5 * (KL_divergence_prob(P, mixture) + KL_divergence_prob(Q, mixture))
+
+
+class MutualInformation:
+    """
+    Class to calculate mutual information between two discrete time series.
+    Currently supports MI calculation using frequency based method only.
+    Estimation methods will be added in future.
+    
+    Parameters
+    ----------
+    time_series_1 : list like
+    time_series_2 : list like
+    bootstrap : bool, False
+    num_of_trials : int, 1000
+    
+    Instance Attributes
+    -------------------
+    mutual_info : float
+    mutual_info_trials : list of float
+    """
+
+    def __init__(self, time_series_1, time_series_2, bootstrap=False, num_of_trials=1000):
+        self.time_series_1 = time_series_1
+        self.time_series_2 = time_series_2
+        self.bootstrap = bootstrap
+        self.num_of_trials = num_of_trials
+        
+        self.mutual_info = self._calculate_mutual_information(self.time_series_1, self.time_series_2)
+
+        if(self.bootstrap):
+            self.mutual_info_trials = self._bootstrap()
+
+    def _calculate_mutual_information(self, time_series_1, time_series_2):
+        """
+        Calculate mutual information between two discrete time series.
+
+        Parameters
+        ----------
+        time_series_1 : list like
+        time_series_2 : list like
+
+        Returns
+        -------
+        float
+        """
+
+        assert len(time_series_1) == len(time_series_2)
+
+        joint_events = list(zip(time_series_1, time_series_2))
+        total_events = len(joint_events)
+
+        joint_prob = Counter(joint_events)
+        joint_prob = {k:v/total_events for k, v in joint_prob.items()}
+        
+        if np.array_equal(self.time_series_1, time_series_1) and np.array_equal(self.time_series_2, time_series_2):
+            self.joint_prob = joint_prob
+
+        prob_time_series_1 = Counter(time_series_1)
+        prob_time_series_2 = Counter(time_series_2)
+
+        mutual_info = 0.0
+
+        for (i, j), prob_joint in joint_prob.items():
+            prob_x = prob_time_series_1[i] / total_events
+            prob_y = prob_time_series_2[j] / total_events
+            mutual_info += prob_joint * np.log2(prob_joint / (prob_x*prob_y))
+
+        return mutual_info
+
+    def _bootstrap(self):
+        """
+        Bootstrap the mutual information calculation.
+
+        Returns
+        -------
+        list of float
+        """
+
+        mutual_info_trials = []
+        for _ in range(self.num_of_trials):
+            time_series_1_boot = np.random.choice(self.time_series_1, len(self.time_series_1), replace=True)
+            time_series_2_boot = np.random.choice(self.time_series_2, len(self.time_series_2), replace=True)
+
+            mutual_info_boot = self._calculate_mutual_information(time_series_1_boot, time_series_2_boot)
+            mutual_info_trials.append(mutual_info_boot)
+        
+        return mutual_info_trials
