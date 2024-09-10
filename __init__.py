07ce8db8549a89c23e719f427c3b1974e1771124
niraj <nirajkkushwaha1@gmail.com>
2024-09-10 16:59:11 +0200

    added module for geometrical operations

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .info_theory import *
 from .list_operations import *
+from .geo_operation import *
---
+++ geo_operation.py
@@ -0,0 +1,36 @@
+from utils import *
+
+def polygons_intersection_mapping(gdf1, gdf2):
+    """
+    Get the mapping of polygons in gdf2 to polygons in gdf1
+    based on intersection area.
+
+    Parameters
+    ----------
+    gdf1 : GeoDataFrame
+    gdf2 : GeoDataFrame
+
+    Returns
+    -------
+    dict
+        A dictionary where keys are the indices of polygons 
+        in gdf2 and values are the indices of polygons in gdf1.
+    """
+
+    intersections = gpd.sjoin(gdf2, gdf1, predicate="intersects")["index_right"]
+
+    adm_mapping = {}
+    for polygon_2_ix in gdf2.index:
+        intersection_areas = {}
+        polygons_1s = intersections.loc[polygon_2_ix]
+
+        if(isinstance(polygons_1s, np.int64)):
+            adm_mapping[polygon_2_ix] = polygons_1s
+        else:
+            for polygon_1_ix in polygons_1s:
+                intersection_areas[polygon_1_ix] = gdf2.geometry.loc[polygon_2_ix].intersection(gdf1.geometry.loc[polygon_1_ix]).area
+
+            adm_mapping[polygon_2_ix] = max(intersection_areas, key=intersection_areas.get)
+            
+    return adm_mapping
+    
