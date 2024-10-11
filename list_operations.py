94f1ccb489e5169c7bab066969e613cd814decb1
Niraj Kushwaha <54101899+NirajKushwaha@users.noreply.github.com>
2024-10-11 17:49:43 +0200

    added function to calculate min and max using parallelization

--- list_operations.py
+++ list_operations.py
@@ -28,3 +28,85 @@ def normalize_to_range(lst, x, y):
     normalized = [(y - x) * (val - min_val) / (max_val - min_val) + x if not np.isnan(val) else np.nan for val in arr]
 
     return np.array(normalized)
+
+def min_in_parallel(array):
+    """
+    Find the minimum of a 1D numpy array using parallelization.
+    
+    Parameters
+    ----------
+    array : 1D ndarray
+    
+    Returns
+    -------
+    float
+    """
+
+    def find_min_in_subarray(subarray, result, idx):
+        min_val = np.min(subarray)
+        result[idx] = min_val
+
+
+    num_processes = int(cpu_count()/2) # Adjust the number of processes as needed
+    chunk_size = len(array) // num_processes
+
+    min_values = multiprocessing.Array('d', num_processes)
+
+    processes = []
+
+    for i in range(num_processes):
+        start_idx = i * chunk_size
+        end_idx = (i + 1) * chunk_size if i < num_processes - 1 else len(array)
+        subarray = array[start_idx:end_idx]
+
+        p = multiprocessing.Process(target=find_min_in_subarray, args=(subarray, min_values, i))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    overall_min = np.min(min_values)
+    
+    return overall_min
+
+def max_in_parallel(array):
+    """
+    Find the maximum of a 1D numpy array using parallelization.
+    
+    Parameters
+    ----------
+    array : 1D ndarray
+    
+    Returns
+    -------
+    float
+    """
+    
+    def find_max_in_subarray(subarray, result, idx):
+        max_val = np.max(subarray)
+        result[idx] = max_val
+
+
+    num_processes = int(cpu_count()/2) # Adjust the number of processes as needed
+    chunk_size = len(array) // num_processes
+
+    max_values = multiprocessing.Array('d', num_processes)
+
+    processes = []
+
+    for i in range(num_processes):
+        start_idx = i * chunk_size
+        end_idx = (i + 1) * chunk_size if i < num_processes - 1 else len(array)
+        subarray = array[start_idx:end_idx]
+
+        p = multiprocessing.Process(target=find_max_in_subarray, args=(subarray, max_values, i))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    overall_max = np.max(max_values)
+    
+    return overall_max
