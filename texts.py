a6619f6acd77370f4ed9d7b863374dff49e8dfce
Niraj Kushwaha <nirajkkushwaha1@gmail.com>
2026-07-14 18:53:56 -0400

    added func to get all unique words

--- texts.py
+++ texts.py
@@ -1,2 +1,22 @@
 from utils import *
 
+def get_unique_words(corpus):
+    """
+    Get all unique words from a corpus of text.
+
+    Parameters
+    ----------
+    corpus : list of str
+        A list of strings representing the text corpus.
+    
+    Returns
+    -------
+    set
+        A set of unique words found in the corpus, converted to lowercase.
+    """
+
+    pattern = re.compile(r"\b\w+\b")
+    unique = set()
+    for text in corpus:
+        unique.update(map(str.lower, pattern.findall(text)))
+    return unique
